895b7b1d2a536e8d8b474647fb317cd18ac748a9
D Blalock <dwb4ke@virginia.edu>
2019-08-07 15:15:41 -0400

    add truncated svd amm impl

--- experiments/python/amm.py
+++ experiments/python/amm.py
@@ -1,7 +1,10 @@
 #!/bin/env/python
 
 import numpy as np
+from sklearn.utils.extmath import randomized_svd
 
+
+# ================================================================ samplings
 
 def _compute_dim_scores(A, B, A_col_norms=None, B_row_norms=None):
     if A_col_norms is None:
@@ -30,7 +33,7 @@ def sketch_sq_deterministic(A, B, d):
     return A[:, keep_idxs] / weights, B[keep_idxs] / weights
 
 
-def main():
+def test_sketch_sq_sample():
     N, M, D = 100, 50, 200
     np.random.seed(1234)
     A = np.random.randint(5, size=(N, D)).astype(np.float32)
@@ -39,7 +42,6 @@ def main():
     AB = A @ B
     orig_frob_sq = np.sum(AB * AB)
 
-    # normed_errs = []
     prev_normed_err = np.inf
     for d in (10, 20, 30, 40, 50):
         A_hat, B_hat = sketch_sq_sample(A, B, d)
@@ -49,10 +51,67 @@ def main():
         err_frob_sq = np.sum(diffs * diffs)
         normed_err_sq = err_frob_sq / orig_frob_sq
         print('d = {}, err = {:.3f}'.format(d, normed_err_sq))
+        assert normed_err_sq < 1.
         assert normed_err_sq < prev_normed_err
         prev_normed_err = normed_err_sq
-        # normed_errs.append(normed_err_sq)
+
+
+# ================================================================ FD methods
+
+def frequent_directions(A):
+    pass
+
+
+def fd_amm_sketches(A, B, d):
+    pass
+
+
+# ================================================================ TruncatedSVD
+
+def svd_sketches(A, B, d, **kwargs):
+    Ua, Sa, VTa = randomized_svd(A, n_components=d, **kwargs)
+    Ub, Sb, VTb = randomized_svd(B, n_components=d, **kwargs)
+
+    # print("truncated svd mat shapes:")
+    # print(Ua.shape, Sa.shape, VTa.shape)
+    # print(Ub.shape, Sb.shape, VTb.shape)
+
+    return (Ua, np.diag(Sa) @ VTa), (Ub, np.diag(Sb) @ VTb)
+
+
+def test_svd_sketches():
+    N, M, D = 100, 80, 50
+    np.random.seed(1234)
+    A = np.random.randint(5, size=(N, D)).astype(np.float32)
+    B = np.random.randint(5, size=(D, M)).astype(np.float32)
+
+    AB = A @ B
+    orig_frob_sq = np.sum(AB * AB)
+
+    prev_normed_err = np.inf
+    # for d in [10]:
+    for d in (1, 2, 4, 8, 16, 32):
+        (Ua, SVTa), (Ub, SVTb) = svd_sketches(A, B, d)
+        AB_hat = Ua @ (SVTa @ Ub) @ SVTb
+
+        # print("fused mats shapes: ")
+        # print(Ua.shape, SVTa.shape, Ub.shape, SVTb.shape)
+
+        diffs = AB - AB_hat
+        err_frob_sq = np.sum(diffs * diffs)
+        normed_err_sq = err_frob_sq / orig_frob_sq
+        print('d = {}, err = {:.5f}'.format(d, normed_err_sq))
+        assert normed_err_sq < 1.
+        assert normed_err_sq < prev_normed_err
+        prev_normed_err = normed_err_sq
+
+
+# ================================================================ main
+
+# def main():
+#     pass
 
 
 if __name__ == '__main__':
-    main()
+    # test_sketch_sq_sample()
+    test_svd_sketches()
