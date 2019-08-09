f62bb544094f9a79203df4e48ae0136d1b997242
D Blalock <dwb4ke@virginia.edu>
2019-08-09 16:51:31 -0400

    add seemingly working co-occurring directions sketches

--- experiments/python/amm.py
+++ experiments/python/amm.py
@@ -96,7 +96,7 @@ def test_svd_sketches():
         prev_normed_err = normed_err_sq
 
 
-# ================================================================ FD methods
+# ================================================================ FD-amm
 
 def frequent_directions(A, d, variant=None):
     N, D = A.shape
@@ -117,9 +117,8 @@ def frequent_directions(A, d, variant=None):
             raise e
         # cutoff = S[d - 1]  # S is returned as a vector, not a diagonal mat
         if variant == 'robust':
-            raise NotImplementedError
+            raise NotImplementedError()
         else:
-            S = S[:d]
             S = np.sqrt((S - S[-1]) ** 2)
             # print("new S shape: ", S.shape)
         H = np.diag(S) @ Vt  # d x D
@@ -167,6 +166,90 @@ def test_fd_amm_sketches():
         assert normed_err_sq < prev_normed_err
         prev_normed_err = normed_err_sq
 
+
+# ================================================================ Co-occurring
+
+def cooccur_sketches(A, B, d):
+    N, D = A.shape
+    B = B.T
+    M, _ = B.shape
+    assert B.shape[1] == D
+
+    X = np.copy(A[:, :d])   # N x d
+    # Y = np.copy(B[:d].T)    # M x d
+    Y = np.copy(B[:, :d])    # M x d
+
+    # mid_idx = d - 2  # does this make it work better for large d? EDIT: nope
+    mid_idx = d // 2
+    ntrailing_zeros = d - mid_idx
+    # print("mid_idx: ", mid_idx)
+    # print("ntrailing_zeros: ", ntrailing_zeros)
+
+    i = 0
+    while i < D:
+        Qx, Rx = np.linalg.qr(X)  # N x d, d x d
+        Qy, Ry = np.linalg.qr(Y)  # M x d, d x d
+        prod = Rx @ Ry.T          # d x d
+        U, S, Vt = np.linalg.svd(prod, full_matrices=False)  # d x d, d, d x d
+        cutoff = S[mid_idx]
+        S = np.sqrt(np.maximum(S - cutoff, 0))
+
+        X = Qx @ (U @ np.diag(S))
+        # Y = Qy @ (Vt @ np.diag(S))
+        Y = Qy @ (Vt.T @ np.diag(S))
+
+        end_dim = min(D, i + ntrailing_zeros)
+        ncols_to_copy = end_dim - i
+        end_col = mid_idx + ncols_to_copy
+
+        # print("S: ", S)
+        # print("i, end_dim, ncols_to_copy, end_col = ",
+        #       i, end_dim, ncols_to_copy, end_col)
+
+        # X[:, mid_idx:end_col] = A[:, i:end_dim]
+        # Y[:, mid_idx:end_col] = B[i:end_dim].T
+        X[:, mid_idx:end_col] = np.copy(A[:, i:end_dim])
+        # Y[:, mid_idx:end_col] = np.copy(B[i:end_dim].T)
+        Y[:, mid_idx:end_col] = np.copy(B[:, i:end_dim])
+        i = end_dim
+
+    return X, Y.T
+
+
+def test_cooccur_sketches():
+    # so this doesn't have monotonically better acc as d increases; seems to
+    # run into issues with d being a large fraction of D, possibly because
+    # then it doesn't have many iterations and it's just zeroing out a ton of
+    # the singular vectors
+
+    N, M, D = 100, 80, 50
+    # N, M, D = 100, 80, 160
+    np.random.seed(1234)
+    A = np.random.randint(5, size=(N, D)).astype(np.float32)
+    B = np.random.randint(5, size=(D, M)).astype(np.float32)
+
+    AB = A @ B
+    orig_frob_sq = np.sum(AB * AB)
+
+    prev_normed_err = np.inf
+    # for d in [4]:
+    for d in (2, 4, 8, 16, 32):
+        # A_hat, B_hat = fd_amm_sketches(A, B, d)
+        A_hat, B_hat = cooccur_sketches(A, B, d)
+        AB_hat = A_hat @ B_hat
+
+        # print("fused mats shapes: ")
+        # print(Ua.shape, SVTa.shape, Ub.shape, SVTb.shape)
+
+        diffs = AB - AB_hat
+        err_frob_sq = np.sum(diffs * diffs)
+        normed_err_sq = err_frob_sq / orig_frob_sq
+        print('d = {}, err = {:.5f}'.format(d, normed_err_sq))
+        assert normed_err_sq < 1.
+        # assert normed_err_sq < prev_normed_err
+        # prev_normed_err = normed_err_sq
+
+
 # ================================================================ main
 
 # def main():
@@ -176,4 +259,5 @@ def test_fd_amm_sketches():
 if __name__ == '__main__':
     # test_sketch_sq_sample()
     # test_svd_sketches()
-    test_fd_amm_sketches()
+    # test_fd_amm_sketches()
+    test_cooccur_sketches()
